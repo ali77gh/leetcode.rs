85f7ab433f7790a42118be8b895d48bf428b3a0c
Ali ghahremani <alighahremani1377@gmail.com>
2025-02-26 10:33:25 +0330

    optimize: 121

--- src/problem_121_best_time_to_buy_and_sell_stock.rs
+++ src/problem_121_best_time_to_buy_and_sell_stock.rs
@@ -1,6 +1,23 @@
 struct Solution;
 impl Solution {
+    // time complexity: O(n)
     pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max = 0;
+        let mut min = prices[0];
+
+        for i in prices {
+            if i < min {
+                min = i;
+            } else if i - min > max {
+                max = i - min;
+            }
+        }
+
+        max
+    }
+
+    // Time complexity: O(nlogn)
+    pub fn max_profit_slow(prices: Vec<i32>) -> i32 {
         let mut max = 0;
 
         for i in 0..prices.len() {
