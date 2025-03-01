439396dbde8e5edba9a3fbe096c8bec082ba61b8
Ali ghahremani <alighahremani1377@gmail.com>
2025-03-01 15:52:47 +0330

    problem: 122

---
+++ src/problem_122_best_time_to_buy_and_sell_stock_ii.rs
@@ -0,0 +1,34 @@
+struct Solution;
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut profit = 0;
+
+        for (i, today) in prices.iter().skip(1).enumerate() {
+            let yesterday = prices[i];
+            if yesterday < *today {
+                profit += today - yesterday;
+            }
+        }
+        profit
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::max_profit(vec![1, 2, 3, 4, 5]), 4);
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(Solution::max_profit(vec![7, 6, 4, 3, 1]), 0);
+    }
+}
