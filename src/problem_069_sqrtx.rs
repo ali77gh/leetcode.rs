219cb76986061eac3ba1b9d583b1a0cf4fb6d903
Ali ghahremani <alighahremani1377@gmail.com>
2025-03-15 12:33:45 +0330

    optimize: 69 (binary search)

--- src/problem_069_sqrtx.rs
+++ src/problem_069_sqrtx.rs
@@ -1,6 +1,23 @@
 struct Solution;
 impl Solution {
+    // O(log(n)) binary search
     pub fn my_sqrt(x: i32) -> i32 {
+        let x = x as usize;
+        let (mut l, mut r) = (1, x);
+
+        while r - l > 1 {
+            let mid = (r + l) / 2;
+            match (mid * mid).cmp(&x) {
+                std::cmp::Ordering::Less => l = mid,
+                std::cmp::Ordering::Greater => r = mid,
+                std::cmp::Ordering::Equal => return mid as i32,
+            }
+        }
+        l as i32
+    }
+
+    // O(n) => 5ms
+    pub fn my_sqrt_brute_force(x: i32) -> i32 {
         let x = x.try_into().unwrap();
         let mut counter = 0usize;
         while counter * counter <= x {
@@ -27,5 +44,10 @@ mod tests {
     #[test]
     fn case_3() {
         assert_eq!(Solution::my_sqrt(2147395600), 46340);
+    }
+
+    #[test]
+    fn case_4() {
+        assert_eq!(Solution::my_sqrt(1), 1);
     }
 }
